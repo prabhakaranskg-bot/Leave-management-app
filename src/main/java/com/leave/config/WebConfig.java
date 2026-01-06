8131f345859f950f9506e9f909eee541555574f1
Prabhakaran G <prabhakaransaai@gmail.com>
2026-01-06 22:55:48 +0530

    cors commit

--- src/main/java/com/leave/config/WebConfig.java
+++ src/main/java/com/leave/config/WebConfig.java
@@ -8,17 +8,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")  // allow all endpoints
-                        .allowedOrigins("http://localhost:4200") // allow Angular
-                        .allowedOrigins("http://lms-fe-bucket.s3-website.ap-south-2.amazonaws.com") // your FE URL
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowCredentials(true);
-            }
-        };
-    }
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // allow all endpoints
+						.allowedOrigins("http://localhost:4200",
+								"http://lms-fe-bucket.s3-website.ap-south-2.amazonaws.com")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
 }
